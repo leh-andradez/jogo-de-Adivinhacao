bd75a5475622d821b529fbb260e20370e4dd5926
leh-andradez <leticia.andrade.faria@escola.pr.gov.br>
2025-02-18 08:41:04 -0300

    jogo

---
+++ jogo de adivinhacao.py
@@ -0,0 +1,16 @@
+print("**********************")
+print("Bem vindo ao Jogo de Adivinhação!")
+print("**********************")
+
+numero_secreto = 1982
+
+chute_str = input("Digite o seu númoro:")
+print("você digitou:", chute_str)
+chute = int(chute_str)
+
+if (numero_secreto ==chute)
+    print("Você acertou!")
+else:
+    print("Você errou!")
+
+print("Fim de Jogo!")
