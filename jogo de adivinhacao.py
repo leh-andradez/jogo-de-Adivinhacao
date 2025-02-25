a0bea7bd586c4e67afdc9c13089aa252f9922506
leh-andradez <leticia.andrade.faria@escola.pr.gov.br>
2025-02-25 07:44:58 -0300

    jogo

--- jogo de adivinhacao.py
+++ jogo de adivinhacao.py
@@ -1,16 +1,31 @@
 print("**********************")
 print("Bem vindo ao Jogo de Adivinhação!")
 print("**********************")
+numero_secreto = 82
+total_de_tentativas = 5
+rodada = 2
 
-numero_secreto = 1982
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-chute_str = input("Digite o seu númoro:")
+chute_str = input("Digite o seu número:")
 print("você digitou:", chute_str)
 chute = int(chute_str)
 
-if (numero_secreto ==chute)
-    print("Você acertou!")
-else:
-    print("Você errou!")
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+    if (acertou):
+        print("Parabéns! Você acertou!")
+    else:
+        if (maior):
+            print("O seu chute é maior que o número secreto!")
+        elif(menor):
+            print("O seu chute é menor que o número secreto!")
+
+            rodada = rodada + 1
+
+
 
 print("Fim de Jogo!")
